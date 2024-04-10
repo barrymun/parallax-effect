5793f4e8d6696142fb3adfad7341c6cfccdb6e99
Neil Murphy <barrymun@tcd.ie>
2024-04-10 21:36:34 +0100

    zoom-in effect on scroll down, and zoom-out effect on scroll up (wip)

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,17 @@
 import "assets/css/main.css";
 import "assets/scss/main.scss";
 
-(async () => {})();
+const handleScroll = () => {
+  const node = document.querySelector(".destination") as HTMLDivElement;
+  node.style.scale = `${0.5 + window.scrollY / 1000}`;
+};
+
+window.addEventListener("unload", () => {
+  console.log("UNLOAD");
+  window.removeEventListener("scroll", handleScroll);
+});
+
+(async () => {
+  console.log("HERE");
+  window.addEventListener("scroll", handleScroll);
+})();
