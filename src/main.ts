87b3c5a519f821ef35ee2a60f733dba2738f9bad
Neil Murphy <barrymun@tcd.ie>
2024-04-11 12:17:15 +0100

    better scaling on zoom in/out

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,17 @@ import "assets/css/main.css";
 import "assets/scss/main.scss";
 
 const handleScroll = () => {
-  const node = document.querySelector(".destination") as HTMLDivElement;
-  node.style.scale = `${0.5 + window.scrollY / 1000}`;
+  const node = document.querySelector(".destination") as HTMLDivElement | null;
+  if (!node) {
+    return;
+  }
+  node.style.scale = `${0.5 + window.scrollY / 4000}`;
 };
 
 window.addEventListener("unload", () => {
-  console.log("UNLOAD");
   window.removeEventListener("scroll", handleScroll);
 });
 
 (async () => {
-  console.log("HERE");
   window.addEventListener("scroll", handleScroll);
 })();
