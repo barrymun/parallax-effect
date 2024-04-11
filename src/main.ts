ca25c283366f7d7f6c98ea8cfedf74fcab302e46
Neil Murphy <barrymun@tcd.ie>
2024-04-11 21:34:57 +0100

    credits and more concise variables names

--- src/main.ts
+++ src/main.ts
@@ -7,19 +7,19 @@ const handleScroll = () => {
   const destination = document.querySelector(".destination") as HTMLDivElement;
   destination.style.scale = `${1 + window.scrollY / scrollOffset}`;
 
-  const foliageOne = document.querySelector(".foliage-1") as HTMLDivElement;
-  foliageOne.style.left = `${-250 - window.scrollY / 10}px`;
+  const f1 = document.querySelector(".foliage-1") as HTMLDivElement;
+  f1.style.left = `${-250 - window.scrollY / 10}px`;
 
-  const foliageTwo = document.querySelector(".foliage-2") as HTMLDivElement;
-  foliageTwo.style.top = `${-250 - window.scrollY / 10}px`;
-  foliageTwo.style.transform = `rotate(${170 - window.scrollY / 100}deg)`;
+  const f2 = document.querySelector(".foliage-2") as HTMLDivElement;
+  f2.style.top = `${-250 - window.scrollY / 10}px`;
+  f2.style.transform = `rotate(${170 - window.scrollY / 100}deg)`;
 
-  const foliageThree = document.querySelector(".foliage-3") as HTMLDivElement;
-  foliageThree.style.top = `${-250 - window.scrollY / 10}px`;
-  foliageThree.style.transform = `rotate(${210 + window.scrollY / 100}deg)`;
+  const f3 = document.querySelector(".foliage-3") as HTMLDivElement;
+  f3.style.top = `${-250 - window.scrollY / 10}px`;
+  f3.style.transform = `rotate(${210 + window.scrollY / 100}deg)`;
 
-  const foliageFour = document.querySelector(".foliage-4") as HTMLDivElement;
-  foliageFour.style.right = `${-250 - window.scrollY / 10}px`;
+  const f4 = document.querySelector(".foliage-4") as HTMLDivElement;
+  f4.style.right = `${-250 - window.scrollY / 10}px`;
 };
 
 window.addEventListener("unload", () => {
