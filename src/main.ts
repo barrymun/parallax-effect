f6658fe0ef789be25064998217e7d4865b1bb697
Neil Murphy <barrymun@tcd.ie>
2024-04-11 13:57:29 +0100

    remove scale from destination, leaves positioned around forest move back and some rotate on scroll down (simulated destination approach), breakpoint handlers for mobile

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,25 @@
 import "assets/css/main.css";
 import "assets/scss/main.scss";
 
-const handleScroll = () => {
-  const node = document.querySelector(".destination") as HTMLDivElement | null;
-  if (!node) {
-    return;
-  }
-  node.style.scale = `${0.75 + window.scrollY / 8000}`;
-};
+import { scrollOffset } from "utils/consts";
 
-const generateFoliage = () => {
-  const node = document.querySelector(".forest") as HTMLDivElement | null;
-  if (!node) {
-    return;
-  }
-  const count = 10;
-  for (let i = 0; i < count; i++) {
-    const leaf = document.createElement("div");
-    leaf.className = `foliage`;
-    leaf.style.top = `calc(50% - ${250 + i * 100}px)`;
-    // leaf.style.left += 0;
-    leaf.style.transform = `rotate(${90 + i * 10}deg)`;
-    node.appendChild(leaf);
-  }
+const handleScroll = () => {
+  const destination = document.querySelector(".destination") as HTMLDivElement;
+  destination.style.scale = `${1 + window.scrollY / scrollOffset}`;
+
+  const foliageOne = document.querySelector(".foliage-1") as HTMLDivElement;
+  foliageOne.style.left = `${-250 - window.scrollY / 10}px`;
+
+  const foliageTwo = document.querySelector(".foliage-2") as HTMLDivElement;
+  foliageTwo.style.top = `${-250 - window.scrollY / 10}px`;
+  foliageTwo.style.transform = `rotate(${170 - window.scrollY / 100}deg)`;
+
+  const foliageThree = document.querySelector(".foliage-3") as HTMLDivElement;
+  foliageThree.style.top = `${-250 - window.scrollY / 10}px`;
+  foliageThree.style.transform = `rotate(${210 + window.scrollY / 100}deg)`;
+
+  const foliageFour = document.querySelector(".foliage-4") as HTMLDivElement;
+  foliageFour.style.right = `${-250 - window.scrollY / 10}px`;
 };
 
 window.addEventListener("unload", () => {
@@ -31,5 +28,4 @@ window.addEventListener("unload", () => {
 
 (async () => {
   window.addEventListener("scroll", handleScroll);
-  generateFoliage();
 })();
