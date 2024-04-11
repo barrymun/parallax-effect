23bf26611d5bf5cd60a771ccc3e3442618bbf728
Neil Murphy <barrymun@tcd.ie>
2024-04-11 12:52:53 +0100

    testing idea with ring of leaves

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,23 @@ const handleScroll = () => {
   if (!node) {
     return;
   }
-  node.style.scale = `${0.5 + window.scrollY / 4000}`;
+  node.style.scale = `${0.75 + window.scrollY / 8000}`;
+};
+
+const generateFoliage = () => {
+  const node = document.querySelector(".forest") as HTMLDivElement | null;
+  if (!node) {
+    return;
+  }
+  const count = 10;
+  for (let i = 0; i < count; i++) {
+    const leaf = document.createElement("div");
+    leaf.className = `foliage`;
+    leaf.style.top = `calc(50% - ${250 + i * 100}px)`;
+    // leaf.style.left += 0;
+    leaf.style.transform = `rotate(${90 + i * 10}deg)`;
+    node.appendChild(leaf);
+  }
 };
 
 window.addEventListener("unload", () => {
@@ -15,4 +31,5 @@ window.addEventListener("unload", () => {
 
 (async () => {
   window.addEventListener("scroll", handleScroll);
+  generateFoliage();
 })();
